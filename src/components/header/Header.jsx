35a59e921b15cb10fd8a97d7104e48d2b9e20d8d
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-08-31 19:35:29 +0700

    header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
@@ -10,6 +12,15 @@ const Header = () => {
         <h1>Farrez Al Hakim</h1>
         <h5 class="text-light">Full Stack Developer</h5>
         <CTA />
+        <HeaderSocial />
+
+        <div class="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
