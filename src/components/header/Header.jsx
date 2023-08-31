36c78d800161a7dfe93db35307cd4b65b1b7756b
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-08-31 18:14:29 +0700

    adding components folders

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return <div>Header 333</div>;
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import ReactDOM from "react-dom";
+import App from "./App";
 
-React
+
+ReactDOM.render(<App />, document.querySelector("#root"));
