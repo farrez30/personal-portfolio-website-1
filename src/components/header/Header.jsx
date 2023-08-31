e2b7d28cfbc954003ff059ac3be5ebccb2dc436d
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-08-31 19:07:04 +0700

    CTA

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div class="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Farrez Al Hakim</h1>
+        <h5 class="text-light">Full Stack Developer</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
