4b299c3d6de8579d346ed8e885e1d4d8c82b8bb7
farrez30 <59773413+farrez30@users.noreply.github.com>
2023-09-01 16:19:34 +0700

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div class="about__me">
+          <div class="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div class="about__content">
+          <div class="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti quibusdam facilis dolorum nihil minima,
+            quisquam aliquam recusandae perspiciatis aspernatur doloremque, eveniet non perferendis! Accusantium, unde
+            accusamus repellendus adipisci quibusdam ullam.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
